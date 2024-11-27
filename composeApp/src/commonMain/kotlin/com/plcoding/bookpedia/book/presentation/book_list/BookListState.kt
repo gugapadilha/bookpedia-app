d31c4c3584e676cd5fa1c6631074a1f0c0661c17
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-11-27 13:35:59 -0300

    git add .

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
@@ -1,3 +1,5 @@
 package com.plcoding.bookpedia.book.presentation.book_list
 
+//MVI Architecture which means that de INTENT is when some UI user action occurred in screens,
+//and then we send to viewmodel, e.g Click in a button, select a book, click in details
 data class BookListState()
