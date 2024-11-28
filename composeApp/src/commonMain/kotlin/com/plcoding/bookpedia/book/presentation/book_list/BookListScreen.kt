8bc19dbd38d8c2de15d009b4e25e8a715a9e475d
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-11-28 13:16:51 -0300

    finished book list screen root, that provides a direct instance of the actual book list screen

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
@@ -2,27 +2,32 @@ package com.plcoding.bookpedia.book.presentation.book_list
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.plcoding.bookpedia.book.domain.Book
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun BookListScreenRoot(
     viewModel: BookListViewModel = koinViewModel(),
-    onBookClick: () -> Unit,
-    modifier: Modifier = Modifier
+    onBookClick: (Book) -> Unit,
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
 
     BookListScreen(
         state = state,
-        onAction = {}
+        onAction = { action ->
+            when(action) {
+                is BookListAction.OnBookClick -> onBookClick(action.book)
+                else -> Unit
+            }
+            viewModel.onAction(action)
+        }
     )
 
 }
 
 @Composable
-fun BookListScreen(
+private fun BookListScreen(
     state: BookListState,
     onAction: (BookListAction) -> Unit
 ) {
