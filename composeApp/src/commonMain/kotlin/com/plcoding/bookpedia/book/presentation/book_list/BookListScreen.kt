1abd479de9165cab9d10017f6104008502b68078
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-11-28 13:10:22 -0300

    create book list screen root to handle viewmodels and di changes

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
@@ -1,9 +1,30 @@
 package com.plcoding.bookpedia.book.presentation.book_list
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
-fun BookListScreen (modifier: Modifier = Modifier) {
+fun BookListScreenRoot(
+    viewModel: BookListViewModel = koinViewModel(),
+    onBookClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    BookListScreen(
+        state = state,
+        onAction = {}
+    )
+
+}
+
+@Composable
+fun BookListScreen(
+    state: BookListState,
+    onAction: (BookListAction) -> Unit
+) {
 
 }
