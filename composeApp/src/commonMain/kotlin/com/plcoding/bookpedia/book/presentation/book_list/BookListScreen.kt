e72a5f76802ccd503afe3ce097246c4716e06a16
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-11-28 13:02:51 -0300

    starting creating book list screen

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
@@ -0,0 +1,9 @@
+package com.plcoding.bookpedia.book.presentation.book_list
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BookListScreen (modifier: Modifier = Modifier) {
+
+}
