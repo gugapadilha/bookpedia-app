86dd23e43da04e53adb40169d8d9301cbac76d28
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-12-02 17:06:56 -0300

    creaed surface to set attributes from books

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookListItem.kt
@@ -1,8 +1,13 @@
 package com.plcoding.bookpedia.book.presentation.book_list.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.core.presentation.LightBlue
 
 @Composable
 fun BookListItem(
@@ -10,5 +15,11 @@ fun BookListItem(
     onClick: () -> Unit,
     modifier: Modifier = Modifier)
 {
-    
+    Surface(
+        shape = RoundedCornerShape(32.dp),
+        modifier = modifier.clickable(onClick = onClick),
+        color = LightBlue.copy(alpha = 0.2f)
+    ){
+
+    }
 }
