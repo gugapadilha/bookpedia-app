ef0e8ed4b98b7a808c05a0118c0666fbe8062b12
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-12-02 17:00:55 -0300

    add parameters to book and onclick inside book

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookListItem.kt
@@ -1,2 +1,14 @@
 package com.plcoding.bookpedia.book.presentation.book_list.components
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.plcoding.bookpedia.book.domain.Book
+
+@Composable
+fun BookListItem(
+    book: Book,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier)
+{
+    
+}
