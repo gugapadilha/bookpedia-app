ab4bdc9db25440382ae33c5bbaa5f6729af3f29d
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-12-03 13:22:03 -0300

    done logic to show book if has or not any image that comes through api

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookListItem.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -19,7 +20,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import cmp_bookpedia.composeapp.generated.resources.Res
+import cmp_bookpedia.composeapp.generated.resources.book_error_2
 import coil3.compose.rememberAsyncImagePainter
 import com.plcoding.bookpedia.book.domain.Book
 import com.plcoding.bookpedia.core.presentation.LightBlue
@@ -29,45 +33,58 @@ import org.jetbrains.compose.resources.painterResource
 fun BookListItem(
     book: Book,
     onClick: () -> Unit,
-    modifier: Modifier = Modifier)
-{
+    modifier: Modifier = Modifier
+) {
     Surface(
         shape = RoundedCornerShape(32.dp),
         modifier = modifier.clickable(onClick = onClick),
         color = LightBlue.copy(alpha = 0.2f)
-    ){
+    ) {
         Row(
             modifier = Modifier
                 .padding(16.dp)
                 .fillMaxWidth()
                 .height(IntrinsicSize.Min) //used to fit the minimum height that we need to fit in any device
-        ){
+        ) {
             Box(
-               modifier = Modifier.height(100.dp),
+                modifier = Modifier.height(100.dp),
                 contentAlignment = Alignment.Center
-            ){
+            ) {
                 var imageLoadResult by remember {
                     mutableStateOf<Result<Painter>?>(null)
                 }
                 val painter = rememberAsyncImagePainter(
                     model = book.imageUrl,
                     onSuccess = {
-                        imageLoadResult = if(it.painter.intrinsicSize.width > 1 && it.painter.intrinsicSize.height > 1){
-                            Result.success(it.painter)
-                        } else {
-                            Result.failure(Exception("Invalid image size"))
-                        }
+                        imageLoadResult =
+                            if (it.painter.intrinsicSize.width > 1 && it.painter.intrinsicSize.height > 1) {
+                                Result.success(it.painter)
+                            } else {
+                                Result.failure(Exception("Invalid image size"))
+                            }
                     },
                     onError = {
                         it.result.throwable.printStackTrace()
                         imageLoadResult = Result.failure(it.result.throwable)
                     }
                 )
-                when(val result = imageLoadResult){
+                when (val result = imageLoadResult) {
                     null -> CircularProgressIndicator()
                     else -> {
                         Image(
-                            painter = if (result.isSuccess) painter else painterResource(R.dr)
+                            painter = if (result.isSuccess) painter else {
+                                painterResource(Res.drawable.book_error_2)
+                            },
+                            contentDescription = book.title,
+                            contentScale = if (result.isSuccess) {
+                                ContentScale.Crop
+                            } else {
+                                ContentScale.Fit
+                            },
+                            modifier = Modifier.aspectRatio(
+                                0.65f,
+                                matchHeightConstraintsFirst = true
+                            )
                         )
                     }
                 }
