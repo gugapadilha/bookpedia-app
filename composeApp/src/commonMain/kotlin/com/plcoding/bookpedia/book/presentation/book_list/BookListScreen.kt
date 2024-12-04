92e273dc2ef70e740f541cb6da09e0d643192b7d
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-12-04 09:14:04 -0300

    create tab to separate all books from favorite

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
@@ -10,10 +10,13 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -21,6 +24,7 @@ import com.plcoding.bookpedia.book.domain.Book
 import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
 import com.plcoding.bookpedia.core.presentation.DarkBlue
 import com.plcoding.bookpedia.core.presentation.DesertWhite
+import com.plcoding.bookpedia.core.presentation.SandYellow
 import org.koin.compose.viewmodel.koinViewModel
 
 //provides a direct instance of the actual book list screen, but deal with navigation and viewmodel
@@ -71,7 +75,26 @@ fun BookListScreen(
                 topStart = 32.dp, topEnd = 32.dp
             )
         ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally){
+                TabRow(
+                    selectedTabIndex = state.selectedTabIndex,
+                    modifier = Modifier.padding(vertical = 12.dp)
+                        .widthIn(max = 700.dp)
+                        .fillMaxWidth()
+                ){
+                    Tab(
+                        selected = state.selectedTabIndex == 0,
+                        onClick = {
+                            onAction(BookListAction.OnTabSelected(0))
+                        },
+                        modifier = Modifier.weight(1f),
+                        selectedContentColor = SandYellow,
+                        unselectedContentColor = Color.Black.copy(alpha = 0.5f)
+                    ){
 
+                    }
+                }
+            }
         }
     }
 }
