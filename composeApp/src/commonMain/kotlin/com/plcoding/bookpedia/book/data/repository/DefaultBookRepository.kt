db6202741ed21917017b9223ee7eb3be651b2a2b
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-12-10 15:04:58 -0300

    create mapper to map the <Book<List>> to SearchResponseDto in network

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/repository/DefaultBookRepository.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/repository/DefaultBookRepository.kt
@@ -1,9 +1,14 @@
 package com.plcoding.bookpedia.book.data.repository
 
 import com.plcoding.bookpedia.book.data.network.RemoteBookDataSource
+import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.core.domain.DataError
+import com.plcoding.bookpedia.core.domain.Result
 
 class DefaultBookRepository(
     private val remoteBookDataSource: RemoteBookDataSource
 ) {
-
+    suspend fun searchBooks(query: String) : Result<List<Book>, DataError.Remote>{
+        return remoteBookDataSource.searchBooks(query)
+    }
 }
