816603c634c4d4c4969e6d5f121028255580eccd
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-12-10 13:29:54 -0300

    added the safe call fun in ktor remote data source to take the api reference

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/KtorRemoteBookDataSource.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/network/KtorRemoteBookDataSource.kt
@@ -4,7 +4,12 @@ import com.plcoding.bookpedia.book.domain.Book
 import com.plcoding.bookpedia.core.domain.DataError
 import com.plcoding.bookpedia.core.domain.DataError.Remote
 import com.plcoding.bookpedia.core.domain.Result
+import com.plcoding.bookpedia.core.domain.data.safeCall
 import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+
+private const val BASE_URL = "https://openlibrary.org"
 
 class KtorRemoteBookDataSource(
     private val httpClient: HttpClient
@@ -13,7 +18,16 @@ class KtorRemoteBookDataSource(
         query: String,
         resultLimit: Int? = null
     ) : Result<List<Book>, DataError.Remote>{
-
+        return safeCall {
+            httpClient.get(
+                urlString = "$BASE_URL/search.json"
+            ) {
+                parameter("q", query)
+                parameter("limit", resultLimit)
+                parameter("language", "eng")
+                parameter("fields", "key,title,language,cover_i,author_key,author_name,cover_edition_key,first_publish_year,ratings_average,ratings_count,number_of_pages_median,edition_count")
+            }
+        }
 
     }
 }
