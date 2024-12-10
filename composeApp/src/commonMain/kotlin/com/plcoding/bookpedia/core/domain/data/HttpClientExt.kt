df1a3c2118ac0f3635df1259c39c2ec03770bbdf
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-12-10 13:21:45 -0300

    create inline fun safecall to prevent http error when execute

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/core/domain/data/HttpClientExt.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/core/domain/data/HttpClientExt.kt
@@ -5,7 +5,27 @@ import com.plcoding.bookpedia.core.domain.DataError
 import io.ktor.client.call.NoTransformationFoundException
 import com.plcoding.bookpedia.core.domain.Result
 import io.ktor.client.call.body
+import io.ktor.client.network.sockets.SocketTimeoutException
 import io.ktor.client.statement.HttpResponse
+import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.coroutines.ensureActive
+import kotlin.coroutines.coroutineContext
+
+suspend inline fun  <reified T> safeCall(
+    execute: () -> HttpResponse
+) : Result<T, DataError.Remote> {
+    val response = try {
+        execute()
+    } catch (e: SocketTimeoutException){
+        return Result.Error(DataError.Remote.REQUEST_TIMEOUT)
+    } catch (e: UnresolvedAddressException) {
+        return Result.Error(DataError.Remote.NO_INTERNET)
+    } catch (e: Exception){
+        coroutineContext.ensureActive()
+        return Result.Error(DataError.Remote.UNKNOWN)
+    }
+    return responseToResult(response)
+}
 
 //REIFIED is used to access the generic type in execution time(normally generic types are
 //extinguish in the compile moments, so, using reified we prevent that.
