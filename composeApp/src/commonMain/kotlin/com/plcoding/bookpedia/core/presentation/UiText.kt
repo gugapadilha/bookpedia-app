21ae853560ec821bddb4ce714f09d48149d93f34
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-12-11 13:35:52 -0300

    string resource to all api errors that might ocurred create, so we dont have to pass every time in the viewmodel

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/core/presentation/UiText.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/core/presentation/UiText.kt
@@ -2,11 +2,12 @@ package com.plcoding.bookpedia.core.presentation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import org.jetbrains.compose.resources.StringResource
 
 sealed interface UiText {
     data class DynamicString(val value: String) : UiText
     class StringResourceId(
-        val id: Int,
+        val id: StringResource,
         val args: Array<Any> = arrayOf()
     ) : UiText
 
