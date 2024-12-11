1762471058f4a600ad5d81b503b4f13be44416f9
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-12-11 13:13:46 -0300

    change val to BookRepository type in book repository, now the presentation layer doesnt have any references of data anymore, just domain

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListViewModel.kt
@@ -2,6 +2,7 @@ package com.plcoding.bookpedia.book.presentation.book_list
 import androidx.lifecycle.ViewModel
 import com.plcoding.bookpedia.book.data.network.KtorRemoteBookDataSource
 import com.plcoding.bookpedia.book.data.repository.DefaultBookRepository
+import com.plcoding.bookpedia.book.domain.BookRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -10,7 +11,7 @@ import kotlinx.coroutines.flow.update
 //If i want to access something from DATA in presentation or domain I need to work with
 //ABSTRACTION to follow the MVVM rule
 class BookListViewModel(
-    private val dataSource : DefaultBookRepository
+    private val dataSource : BookRepository
 ): ViewModel() {
     private val _state = MutableStateFlow(BookListState())
     val state = _state.asStateFlow()
