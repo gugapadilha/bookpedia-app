21777f25425943891f7c22a9190d23bcf648af85
gugapadilha <guga.santospadilha@rede.ulbra.br>
2024-12-16 14:02:03 -0300

    navigation working just fine with serializable as well

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/app/App.kt
@@ -1,9 +1,17 @@
 package com.plcoding.bookpedia.app
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.plcoding.bookpedia.book.presentation.book_list.BookListScreenRoot
 import com.plcoding.bookpedia.book.presentation.book_list.BookListViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -17,18 +25,40 @@ fun App() {
         val navController = rememberNavController()
         NavHost(
             navController,
-            startDestination =
-        )
-        val viewModel = koinViewModel<BookListViewModel>()
+            startDestination = Route.BookGraph
+        ) {
+            navigation<Route.BookGraph>(
+                startDestination = Route.BookList
+            ) {
+                composable<Route.BookList> {
+                    val viewModel = koinViewModel<BookListViewModel>()
+                    BookListScreenRoot(
+                        viewModel = viewModel,
+                        onBookClick = { book ->
+                            navController.navigate(
+                                Route.BookDetail(book.id)
+                            )
 
-        BookListScreenRoot(
-            viewModel = viewModel ,
-            onBookClick = {
+                        }
+                    )
+                }
+                composable<Route.BookDetail> { entry ->
+                    val args = entry.toRoute<Route.BookDetail>()
 
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("Book Detail screen! The id is ${args.id}")
+                    }
+
+                }
             }
-        )
+
+        }
+
     }
-    }
+}
 
 
 // Presentation -> Domain <- Data
