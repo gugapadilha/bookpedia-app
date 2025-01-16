67a95ce34f33106ddfe5598505809f1974c555b6
gugapadilha <guga.santospadilha@rede.ulbra.br>
2025-01-16 12:05:12 -0300

    create row to rating and pages

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/BookDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/BookDetailScreen.kt
@@ -1,12 +1,17 @@
 package com.plcoding.bookpedia.book.presentation.book_detail
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,7 +21,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import cmp_bookpedia.composeapp.generated.resources.Res
+import cmp_bookpedia.composeapp.generated.resources.pages
+import cmp_bookpedia.composeapp.generated.resources.rating
 import com.plcoding.bookpedia.book.presentation.book_detail.components.BlurredImageBackground
+import com.plcoding.bookpedia.book.presentation.book_detail.components.BookChip
+import com.plcoding.bookpedia.book.presentation.book_detail.components.TitledContent
+import com.plcoding.bookpedia.core.presentation.SandYellow
+import org.jetbrains.compose.resources.stringResource
+import kotlin.math.round
 
 @Composable
 fun BookDetailScreenRoot(
@@ -73,6 +86,38 @@ private fun BookDetailScreen(
                     style = MaterialTheme.typography.headlineMedium,
                     textAlign = TextAlign.Center
                 )
+                Row(
+                    modifier = Modifier
+                        .padding(vertical = 8.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    state.book.averageRating?.let { rating ->
+                        TitledContent(
+                            title = stringResource(Res.string.rating),
+                        ) {
+                            BookChip(modifier = Modifier) {
+                                Text(
+                                    text = "${round( rating * 10) / 10.0}"
+                                )
+                                Icon(imageVector = Icons.Default.Star,
+                                    contentDescription = null,
+                                    tint = SandYellow
+                                )
+                            }
+                        }
+                    }
+                    state.book.numPages?.let { pageCount ->
+                        TitledContent(
+                            title = stringResource(Res.string.pages),
+                        ) {
+                            BookChip(modifier = Modifier) {
+                                Text(
+                                    text = pageCount.toString()
+                                )
+                            }
+                        }
+                    }
+                }
             }
         }
 
