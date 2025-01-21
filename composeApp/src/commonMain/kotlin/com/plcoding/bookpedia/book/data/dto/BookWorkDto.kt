2e652b047410631fcb836aee2a03a1b2d36e1ab5
gugapadilha <guga.santospadilha@rede.ulbra.br>
2025-01-21 14:30:02 -0300

    passing book work dto serializer to book work dto to automatically deseriliaze json when necessary

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/dto/BookWorkDto.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/dto/BookWorkDto.kt
@@ -2,7 +2,7 @@ package com.plcoding.bookpedia.book.data.dto
 
 import kotlinx.serialization.Serializable
 
-@Serializable()
+@Serializable(with = BookWorkDtoSerializer::class)
 data class BookWorkDto(
     val description: String? = null
 )
