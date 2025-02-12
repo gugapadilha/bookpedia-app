218f37dae7f6e9fc1dd00b8609f28f1f825a2074
gugapadilha <guga.santospadilha@rede.ulbra.br>
2025-02-12 13:58:24 -0300

    create favorite database schema

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/database/FavoriteBookDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/database/FavoriteBookDatabase.kt
@@ -1,7 +1,12 @@
 package com.plcoding.bookpedia.book.data.database
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
 
+@Database(
+    entities = [BookEntity::class],
+    version = 1
+)
 
 abstract class FavoriteBookDatabase: RoomDatabase() {
 }
