9b23311bceb480e50eacc732e38a3c35ed3f5244
gugapadilha <guga.santospadilha@rede.ulbra.br>
2025-02-12 14:05:50 -0300

    create bookdata base constructor is a must when dealing with kmm, so its basically retrieving data

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/database/FavoriteBookDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/database/FavoriteBookDatabase.kt
@@ -2,12 +2,16 @@ package com.plcoding.bookpedia.book.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(
     entities = [BookEntity::class],
     version = 1
 )
 
+@TypeConverters(
+    StringListTypeConverter::class
+)
 abstract class FavoriteBookDatabase: RoomDatabase() {
     abstract val favoriteBookDao: FavoriteBookDao
 
